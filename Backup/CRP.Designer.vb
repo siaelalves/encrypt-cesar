d8202c6f5ca3eb724e53b73e2cd2c0df488e260c
Siael Alves <32615727+siaelalves@users.noreply.github.com>
2022-01-12 15:40:34 -0300

    Add files via upload

---
+++ Backup/CRP.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CRP
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdCr = New System.Windows.Forms.Button
+        Me.cmdDc = New System.Windows.Forms.Button
+        Me.cmdSvDr = New System.Windows.Forms.Button
+        Me.cmdSvCr = New System.Windows.Forms.Button
+        Me.cmdIpCr = New System.Windows.Forms.Button
+        Me.cmdIpDr = New System.Windows.Forms.Button
+        Me.cmdLmDr = New System.Windows.Forms.Button
+        Me.cmdLmCr = New System.Windows.Forms.Button
+        Me.lblTxDr = New System.Windows.Forms.Label
+        Me.lblTxCr = New System.Windows.Forms.Label
+        Me.txtCr = New System.Windows.Forms.RichTextBox
+        Me.txtDr = New System.Windows.Forms.RichTextBox
+        Me.st = New System.Windows.Forms.StatusStrip
+        Me.prgSt = New System.Windows.Forms.ToolStripProgressBar
+        Me.lblPrVl = New System.Windows.Forms.ToolStripStatusLabel
+        Me.cmdCn = New System.Windows.Forms.Button
+        Me.dlgSv = New System.Windows.Forms.SaveFileDialog
+        Me.cmdAb = New System.Windows.Forms.Button
+        Me.dlgAb = New System.Windows.Forms.OpenFileDialog
+        Me.st.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cmdCr
+        '
+        Me.cmdCr.Location = New System.Drawing.Point(255, 41)
+        Me.cmdCr.Name = "cmdCr"
+        Me.cmdCr.Size = New System.Drawing.Size(75, 23)
+        Me.cmdCr.TabIndex = 2
+        Me.cmdCr.Text = "&Cript >"
+        Me.cmdCr.UseVisualStyleBackColor = True
+        '
+        'cmdDc
+        '
+        Me.cmdDc.Location = New System.Drawing.Point(255, 70)
+        Me.cmdDc.Name = "cmdDc"
+        Me.cmdDc.Size = New System.Drawing.Size(75, 23)
+        Me.cmdDc.TabIndex = 3
+        Me.cmdDc.Text = "< &Decript"
+        Me.cmdDc.UseVisualStyleBackColor = True
+        '
+        'cmdSvDr
+        '
+        Me.cmdSvDr.Location = New System.Drawing.Point(12, 364)
+        Me.cmdSvDr.Name = "cmdSvDr"
+        Me.cmdSvDr.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSvDr.TabIndex = 4
+        Me.cmdSvDr.Text = "&Salvar"
+        Me.cmdSvDr.UseVisualStyleBackColor = True
+        '
+        'cmdSvCr
+        '
+        Me.cmdSvCr.Location = New System.Drawing.Point(498, 364)
+        Me.cmdSvCr.Name = "cmdSvCr"
+        Me.cmdSvCr.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSvCr.TabIndex = 5
+        Me.cmdSvCr.Text = "Sal&var"
+        Me.cmdSvCr.UseVisualStyleBackColor = True
+        '
+        'cmdIpCr
+        '
+        Me.cmdIpCr.Location = New System.Drawing.Point(417, 364)
+        Me.cmdIpCr.Name = "cmdIpCr"
+        Me.cmdIpCr.Size = New System.Drawing.Size(75, 23)
+        Me.cmdIpCr.TabIndex = 6
+        Me.cmdIpCr.Text = "Im&primir"
+        Me.cmdIpCr.UseVisualStyleBackColor = True
+        '
+        'cmdIpDr
+        '
+        Me.cmdIpDr.Location = New System.Drawing.Point(93, 364)
+        Me.cmdIpDr.Name = "cmdIpDr"
+        Me.cmdIpDr.Size = New System.Drawing.Size(75, 23)
+        Me.cmdIpDr.TabIndex = 7
+        Me.cmdIpDr.Text = "&Imprimir"
+        Me.cmdIpDr.UseVisualStyleBackColor = True
+        '
+        'cmdLmDr
+        '
+        Me.cmdLmDr.Location = New System.Drawing.Point(174, 364)
+        Me.cmdLmDr.Name = "cmdLmDr"
+        Me.cmdLmDr.Size = New System.Drawing.Size(75, 23)
+        Me.cmdLmDr.TabIndex = 8
+        Me.cmdLmDr.Text = "&Limpar"
+        Me.cmdLmDr.UseVisualStyleBackColor = True
+        '
+        'cmdLmCr
+        '
+        Me.cmdLmCr.Location = New System.Drawing.Point(336, 364)
+        Me.cmdLmCr.Name = "cmdLmCr"
+        Me.cmdLmCr.Size = New System.Drawing.Size(75, 23)
+        Me.cmdLmCr.TabIndex = 9
+        Me.cmdLmCr.Text = "Li&mpar"
+        Me.cmdLmCr.UseVisualStyleBackColor = True
+        '
+        'lblTxDr
+        '
+        Me.lblTxDr.AutoSize = True
+        Me.lblTxDr.Location = New System.Drawing.Point(12, 25)
+        Me.lblTxDr.Name = "lblTxDr"
+        Me.lblTxDr.Size = New System.Drawing.Size(119, 13)
+        Me.lblTxDr.TabIndex = 10
+        Me.lblTxDr.Text = "Texto descriptografado:"
+        '
+        'lblTxCr
+        '
+        Me.lblTxCr.AutoSize = True
+        Me.lblTxCr.Location = New System.Drawing.Point(333, 25)
+        Me.lblTxCr.Name = "lblTxCr"
+        Me.lblTxCr.Size = New System.Drawing.Size(102, 13)
+        Me.lblTxCr.TabIndex = 11
+        Me.lblTxCr.Text = "Texto criptografado:"
+        '
+        'txtCr
+        '
+        Me.txtCr.Location = New System.Drawing.Point(336, 41)
+        Me.txtCr.Name = "txtCr"
+        Me.txtCr.Size = New System.Drawing.Size(237, 317)
+        Me.txtCr.TabIndex = 1
+        Me.txtCr.Text = ""
+        '
+        'txtDr
+        '
+        Me.txtDr.Location = New System.Drawing.Point(12, 41)
+        Me.txtDr.Name = "txtDr"
+        Me.txtDr.Size = New System.Drawing.Size(237, 317)
+        Me.txtDr.TabIndex = 0
+        Me.txtDr.Text = ""
+        '
+        'st
+        '
+        Me.st.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.prgSt, Me.lblPrVl})
+        Me.st.Location = New System.Drawing.Point(0, 402)
+        Me.st.Name = "st"
+        Me.st.Size = New System.Drawing.Size(587, 22)
+        Me.st.TabIndex = 12
+        Me.st.Text = "StatusStrip1"
+        '
+        'prgSt
+        '
+        Me.prgSt.Name = "prgSt"
+        Me.prgSt.Size = New System.Drawing.Size(100, 16)
+        Me.prgSt.Step = 1
+        '
+        'lblPrVl
+        '
+        Me.lblPrVl.Name = "lblPrVl"
+        Me.lblPrVl.Size = New System.Drawing.Size(23, 17)
+        Me.lblPrVl.Text = "0%"
+        '
+        'cmdCn
+        '
+        Me.cmdCn.Location = New System.Drawing.Point(255, 99)
+        Me.cmdCn.Name = "cmdCn"
+        Me.cmdCn.Size = New System.Drawing.Size(75, 23)
+        Me.cmdCn.TabIndex = 13
+        Me.cmdCn.Text = "Ca&ncelar"
+        Me.cmdCn.UseVisualStyleBackColor = True
+        '
+        'dlgSv
+        '
+        Me.dlgSv.DefaultExt = "crp"
+        Me.dlgSv.FileName = "Encrypted file"
+        Me.dlgSv.Filter = "SAK Encryption (*.crp)|*.crp"
+        Me.dlgSv.RestoreDirectory = True
+        Me.dlgSv.Title = "Salvar texto"
+        '
+        'cmdAb
+        '
+        Me.cmdAb.Location = New System.Drawing.Point(255, 128)
+        Me.cmdAb.Name = "cmdAb"
+        Me.cmdAb.Size = New System.Drawing.Size(75, 23)
+        Me.cmdAb.TabIndex = 14
+        Me.cmdAb.Text = "&Abrir"
+        Me.cmdAb.UseVisualStyleBackColor = True
+        '
+        'dlgAb
+        '
+        Me.dlgAb.DefaultExt = "crp"
+        Me.dlgAb.FileName = "Encrypted file"
+        Me.dlgAb.Filter = "SAK Encryption (*.crp)|*.crp"
+        Me.dlgAb.RestoreDirectory = True
+        '
+        'CRP
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(587, 424)
+        Me.Controls.Add(Me.cmdAb)
+        Me.Controls.Add(Me.cmdCn)
+        Me.Controls.Add(Me.st)
+        Me.Controls.Add(Me.lblTxCr)
+        Me.Controls.Add(Me.lblTxDr)
+        Me.Controls.Add(Me.cmdLmCr)
+        Me.Controls.Add(Me.cmdLmDr)
+        Me.Controls.Add(Me.cmdIpDr)
+        Me.Controls.Add(Me.cmdIpCr)
+        Me.Controls.Add(Me.cmdSvCr)
+        Me.Controls.Add(Me.cmdSvDr)
+        Me.Controls.Add(Me.cmdDc)
+        Me.Controls.Add(Me.cmdCr)
+        Me.Controls.Add(Me.txtCr)
+        Me.Controls.Add(Me.txtDr)
+        Me.Name = "CRP"
+        Me.Text = "SAK Cript"
+        Me.st.ResumeLayout(False)
+        Me.st.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmdCr As System.Windows.Forms.Button
+    Friend WithEvents cmdDc As System.Windows.Forms.Button
+    Friend WithEvents cmdSvDr As System.Windows.Forms.Button
+    Friend WithEvents cmdSvCr As System.Windows.Forms.Button
+    Friend WithEvents cmdIpCr As System.Windows.Forms.Button
+    Friend WithEvents cmdIpDr As System.Windows.Forms.Button
+    Friend WithEvents cmdLmDr As System.Windows.Forms.Button
+    Friend WithEvents cmdLmCr As System.Windows.Forms.Button
+    Friend WithEvents lblTxDr As System.Windows.Forms.Label
+    Friend WithEvents lblTxCr As System.Windows.Forms.Label
+    Friend WithEvents txtCr As System.Windows.Forms.RichTextBox
+    Friend WithEvents txtDr As System.Windows.Forms.RichTextBox
+    Friend WithEvents st As System.Windows.Forms.StatusStrip
+    Friend WithEvents prgSt As System.Windows.Forms.ToolStripProgressBar
+    Friend WithEvents lblPrVl As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents cmdCn As System.Windows.Forms.Button
+    Friend WithEvents dlgSv As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents cmdAb As System.Windows.Forms.Button
+    Friend WithEvents dlgAb As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ CRP.vb
@@ -0,0 +1,332 @@
+﻿Public Class CRP
+    Shared tc As Int32
+    Shared Property TickCount() As Int32
+        Get
+            Return tc
+        End Get
+        Set(ByVal value As Int32)
+            tc = value
+        End Set
+    End Property
+
+    Private Sub cmdCr_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdCr.Click
+        Me.Text = "Criptografando"
+        txtCr.Text = Nothing
+        Cursor = Cursors.WaitCursor
+        prgSt.Maximum = Criptografia.Texto.Length
+        For Each c As Char In Criptografia.Texto
+            If AscW(c) = 10 Then
+                txtCr.Text = txtCr.Text & "#pg"
+            Else
+                txtCr.Text = txtCr.Text & ChrW(AscW(c) + 17) & ChrW(AscW(c) + 34) & ChrW(AscW(c) + 51) & _
+                ChrW(AscW(c) + 68) & ChrW(AscW(c) + 85)
+            End If
+            prgSt.PerformStep()
+            lblPrVl.Text = prgSt.Value & "%"
+        Next
+        lblPrVl.Text = "0%"
+        Cursor = Cursors.Default
+        prgSt.Value = 0
+        Me.Text = My.Application.Info.Title
+    End Sub
+
+    Private Sub cmdDc_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdDc.Click
+        Me.Text = "Descriptografando"
+        txtDr.Text = Nothing
+        Cursor = Cursors.WaitCursor
+        Dim c() As Char = Descriptografia.Texto
+        prgSt.Maximum = Descriptografia.Texto.Length
+        For i = 0 To UBound(c)
+            If i + 1 < UBound(c) Then
+                Do
+                    If Chr(Asc(c(i))) = "#" And _
+                        Chr(Asc(c(i + 1))) = "p" And _
+                        Chr(Asc(c(i + 2))) = "g" Then
+                        txtDr.Text = txtDr.Text & Chr(13)
+                        If i < UBound(c) - 3 Then
+                            i += 3
+                        Else
+                            Exit For
+                        End If
+                    End If
+                Loop Until (Chr(Asc(c(i))) = "#" And _
+                    Chr(Asc(c(i + 1))) = "p" And _
+                    Chr(Asc(c(i + 2))) = "g") = False
+            End If
+            txtDr.Text = txtDr.Text & ChrW(AscW(c(i)) - 17)
+            i += 4
+            prgSt.PerformStep()
+            lblPrVl.Text = prgSt.Value & "%"
+        Next
+        Cursor = Cursors.Default
+        lblPrVl.Text = "0%"
+        prgSt.Value = 0
+        Me.Text = My.Application.Info.Title
+    End Sub
+
+    Private Sub cmdAb_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdAb.Click
+        dlgAb.Title = "Abrir"
+        If dlgAb.ShowDialog = Windows.Forms.DialogResult.OK Then
+            If Arquivo.Versão(dlgAb.FileName) = Programa.Versão Then
+                If Arquivo.Criador(dlgAb.FileName) = Programa.Nome Then
+                    If Arquivo.Tipo(dlgAb.FileName) = 0 Then
+                        Dim d() As String = IO.File.ReadAllLines(dlgAb.FileName, System.Text.Encoding.Default)
+                        txtCr.Text = ""
+                        For i = 1 To UBound(d)
+                            For Each c As Char In d(i)
+                                txtCr.Text = txtCr.Text & c
+                            Next c
+                        Next i
+                    ElseIf Arquivo.Tipo(dlgAb.FileName) = 1 Then
+                        Dim d() As String = IO.File.ReadAllLines(dlgAb.FileName, System.Text.Encoding.Default)
+                        txtDr.Text = ""
+                        For i = 1 To UBound(d)
+                            For Each c As Char In d(i)
+                                txtDr.Text = txtDr.Text & c
+                            Next c
+                        Next i
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub cmdLmCr_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdLmCr.Click
+        txtCr.Text = ""
+    End Sub
+
+    Private Sub cmdLmDr_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdLmDr.Click
+        txtDr.Text = ""
+    End Sub
+
+    Private Sub cmdSvDr_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSvDr.Click
+        dlgSv.Title = "Salvar texto descriptografado"
+        If dlgSv.ShowDialog = Windows.Forms.DialogResult.OK Then
+            FileOpen(1, dlgSv.FileName, 2)
+            PrintLine(1, "type=decrypted program=" & Programa.Nome & " version=" & _
+                      Programa.Versão & " date=" & Date.Today)
+            Print(1, txtDr.Text)
+            FileClose(1)
+        End If
+    End Sub
+
+    Private Sub cmdSvCr_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSvCr.Click
+        dlgSv.Title = "Salvar texto criptografado"
+        If dlgSv.ShowDialog = Windows.Forms.DialogResult.OK Then
+            FileOpen(1, dlgSv.FileName, 2)
+            For Each c As Char In ("type=decrypted program=" & Programa.Nome & " version=" & _
+                      Programa.Versão & " date=" & Date.Today)
+                Print(1, ChrW(AscW(c) + 17) & ChrW(AscW(c) + 34) & ChrW(AscW(c) + 51) & _
+                ChrW(AscW(c) + 68) & ChrW(AscW(c) + 85))
+            Next
+            PrintLine(1)
+            Dim d() As Char = txtCr.Text
+            For i = 0 To UBound(d)
+                If i + 1 < UBound(d) Then
+                    Do
+                        If d(i) = "#" And _
+                            d(i + 1) = "p" And _
+                            d(i + 2) = "g" = True Then
+                            PrintLine(1)
+                            i += 1
+                        Else
+                            Exit Do
+                        End If
+                    Loop Until d(i) = "#" And _
+                    d(i + 1) = "p" And _
+                    d(i + 2) = "g" = False
+                End If
+                Print(1, d(i))
+            Next i
+            FileClose(1)
+        End If
+    End Sub
+    ''' <summary>
+    ''' Permite acesso a informações básicas sobre o programa em uso.
+    ''' </summary>
+    ''' <remarks></remarks>
+    Class Programa
+        ''' <summary>
+        ''' Retorna a versão do programa que está sendo usado.
+        ''' </summary>
+        Shared ReadOnly Property Versão() As String
+            Get
+                Return My.Application.Info.Version.Major & "." & My.Application.Info.Version.Minor & "." & _
+                        My.Application.Info.Version.Build & "." & My.Application.Info.Version.Revision
+            End Get
+        End Property
+        Shared ReadOnly Property Nome() As String
+            Get
+                Return My.Application.Info.Title
+            End Get
+        End Property
+    End Class
+    ''' <summary>
+    ''' Permite acesso à leitura de metadados do arquivo.
+    ''' </summary>
+    ''' <remarks></remarks>
+    Class Arquivo
+        Enum TipoDeArquivo
+            Encrypted = 0
+            Decrypted = 1
+        End Enum
+        ''' <summary>
+        ''' Retorna se o tipo do arquivo selecionado é criptografado ou não.
+        ''' </summary>
+        ''' <param name="arquivo">Caminho completo do arquivo.</param>
+        Shared ReadOnly Property Tipo(ByVal arquivo As String) As TipoDeArquivo
+            Get
+                Dim d() As String = IO.File.ReadAllLines(arquivo, System.Text.Encoding.Default)
+                For i = 0 To UBound(d)
+                    If d(i).ToLower.StartsWith("type=") = False Then
+                        Return 0
+                    End If
+                    Dim n() As String
+                    n = Split(d(i), " ")
+                    Dim p() As String
+                    For j = 0 To UBound(n)
+                        p = Split(n(0), "=")
+                        If Trim(p(1)).ToLower = "decrypted" Then
+                            Return 1
+                        End If
+                    Next
+                Next
+            End Get
+        End Property
+        ''' <summary>
+        ''' Retorna o criador do arquivo, i.e., quais dos sistemas SAKCript gerou o arquivo.
+        ''' </summary>
+        ''' <param name="arquivo">Caminho completo do arquivo.</param>
+        Shared ReadOnly Property Criador(ByVal arquivo As String) As String
+            Get
+                If Tipo(arquivo) = 1 Then
+                    Dim d() As String = IO.File.ReadAllLines(arquivo, System.Text.Encoding.Default)
+                    For i = 0 To UBound(d)
+                        Dim n() As String
+                        n = Split(d(i), " ")
+                        Dim p() As String
+                        For j = 0 To UBound(n)
+                            p = Split(n(1), "=")
+                            Return Trim(p(1))
+                        Next
+                    Next
+                ElseIf Tipo(arquivo) = 0 Then
+                    Dim d() As String = IO.File.ReadAllLines(arquivo, System.Text.Encoding.Default)
+                    For i = 0 To UBound(d)
+                        Dim n() As String
+                        n = Split(d(i), ChrW(AscW(" ") + 17) & ChrW(AscW(" ") + 34) & ChrW(AscW(" ") + 51) & _
+                ChrW(AscW(" ") + 68) & ChrW(AscW(" ") + 85))
+                        Dim p() As String
+                        For j = 0 To UBound(n)
+                            p = Split(n(1), ChrW(AscW("=") + 17) & ChrW(AscW("=") + 34) & ChrW(AscW("=") + 51) & _
+                ChrW(AscW("=") + 68) & ChrW(AscW("=") + 85))
+                            Return Trim(p(1))
+                        Next
+                    Next
+                End If
+            End Get
+        End Property
+        ''' <summary>
+        ''' Retorna a versão do programa usada para criar o arquivo.
+        ''' </summary>
+        ''' <param name="arquivo">Caminho completo do arquivo.</param>
+        ''' <value></value>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Shared ReadOnly Property Versão(ByVal arquivo As String) As String
+            Get
+                If Tipo(arquivo) = 1 Then
+                    Dim d() As String = IO.File.ReadAllLines(arquivo, System.Text.Encoding.Default)
+                    For i = 0 To UBound(d)
+                        Dim n() As String
+                        n = Split(d(i), " ")
+                        Dim p() As String
+                        For j = 0 To UBound(n)
+                            p = Split(n(2), "=")
+                            Return Trim(p(1))
+                        Next
+                    Next
+                ElseIf Tipo(arquivo) = 0 Then
+                    Dim d() As String = IO.File.ReadAllLines(arquivo, System.Text.Encoding.Unicode)
+                    For i = 0 To UBound(d)
+                        Dim n() As String
+                        n = Split(d(i), ChrW(AscW(" ") + 17) & ChrW(AscW(" ") + 34) & ChrW(AscW(" ") + 51) & _
+                            ChrW(AscW(" ") + 68) & ChrW(AscW(" ") + 85))
+                        Dim p() As String
+                        p = Split(n(2), ChrW(AscW("=") + 17) & ChrW(AscW("=") + 34) & ChrW(AscW("=") + 51) & _
+                            ChrW(AscW("=") + 68) & ChrW(AscW("=") + 85))
+                        Return p(1)
+                    Next
+                End If
+            End Get
+        End Property
+        ''' <summary>
+        ''' Retorna a data de criação do arquivo.
+        ''' </summary>
+        ''' <param name="arquivo">Caminho completo do arquivo.</param>
+        ''' <value></value>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Shared ReadOnly Property Data(ByVal arquivo As String) As Date
+            Get
+                If Tipo(arquivo) = 1 Then
+                    Dim d() As String = IO.File.ReadAllLines(arquivo, System.Text.Encoding.Default)
+                    For i = 0 To UBound(d)
+                        Dim n() As String
+                        n = Split(d(i), " ")
+                        Dim p() As String
+                        For j = 0 To UBound(n)
+                            p = Split(n(3), "=")
+                            Return Convert.ToDateTime(Trim(p(1)))
+                        Next
+                    Next
+                ElseIf Tipo(arquivo) = 0 Then
+                    Dim d() As String = IO.File.ReadAllLines(arquivo, System.Text.Encoding.Default)
+                    For i = 0 To UBound(d)
+                        Dim n() As String
+                        n = Split(d(i), ChrW(AscW(" ") + 17) & ChrW(AscW(" ") + 34) & ChrW(AscW(" ") + 51) & _
+                ChrW(AscW(" ") + 68) & ChrW(AscW(" ") + 85))
+                        Dim p() As String
+                        For j = 0 To UBound(n)
+                            p = Split(n(3), ChrW(AscW("=") + 17) & ChrW(AscW("=") + 34) & ChrW(AscW("=") + 51) & _
+                ChrW(AscW("=") + 68) & ChrW(AscW("=") + 85))
+                            Return Trim(p(1))
+                        Next
+                    Next
+                End If
+            End Get
+        End Property
+    End Class
+    Class Descriptografia
+        Shared dtx As String
+        ''' <summary>
+        ''' Retorna o conteúdo do campo "Texto criptografado".
+        ''' </summary>
+        ''' <returns>Retorna todo o texto que será descriptografado.</returns>
+        ''' <remarks></remarks>
+        Shared Property Texto() As String
+            Get
+                Return CRP.txtCr.Text
+            End Get
+            Set(ByVal value As String)
+                dtx = value
+            End Set
+        End Property
+    End Class
+    Class Criptografia
+        Shared ctx As String
+        ''' <summary>
+        ''' Retorna o conteúdo do campo "Texto descriptografado".
+        ''' </summary>
+        ''' <returns>Retorna todo o texto que será criptografado.</returns>
+        ''' <remarks></remarks>
+        Shared Property Texto() As String
+            Get
+                Return CRP.txtDr.Text
+            End Get
+            Set(ByVal value As String)
+                ctx = value
+            End Set
+        End Property
+    End Class
+End Class
